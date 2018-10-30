b1bb2b8a6390aff2c11e9f72b740bf12449e79df
t:elday <ejw393@gmail.com>
2018-10-30 08:30:11 -0400

    Added function to extract reddit post names from links

--- bot.js
+++ bot.js
@@ -3,6 +3,15 @@ const fs = require('fs');
 const rp = require('request-promise');
 const client = new Discord.Client();
 
+function extractThreadName(text){
+	let pattern = new RegExp("(comments\/*)[a-z A-Z 0-9]+");
+	let pattern2 = new RegExp("(\/)[a-z A-Z 0-9]+");
+	let matching = text.match(pattern);
+	matching = matching[0].match(pattern2)[0];
+	matching = matching.slice(1);
+	return matching;
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -11,7 +20,10 @@ client.on('message', msg => {
 	let k = 'https://www.reddit.com/by_id/t3_9semdw.json';
 	rp({uri:k, json:true, raw_json:1})
 		.then(function(res) {
-				console.log(res['data'].children[0].data.url);
+			let name = extractThreadName(msg.content);
+
+			console.log(url[0]);
+			//console.log(res['data'].children[0].data.url);
 		})
 		.catch(function(res) {
 			console.log('fail');
